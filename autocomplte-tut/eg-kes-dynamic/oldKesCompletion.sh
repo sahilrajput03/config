f83fe25cfd30cb2762a0e75d08037860aaef7f2f
Sahil Rajput <sahilrajput03@gmail.com>
2022-04-11 16:19:49 +0530

    up

--- autocomplte-tut/eg-kes-dynamic/oldKesCompletion.sh
+++ autocomplte-tut/eg-kes-dynamic/oldKesCompletion.sh
@@ -1,24 +1,29 @@
 #/usr/bin/env bash
 
-# Usage: 
-# kes pod-name container-name
-
-# IMPORATNT: ARGUMENST PASSED TO FUNCTION VIA THE COMPLETION ENVIRONEMNT: ( Source: https://www.gnu.org/software/bash/manual/html_node/Programmable-Completion-Builtins.html#Programmable-Completion-Builtins )
-# IMPORTANT: $1 is the name of the command whose arguments are being completed
-# IPMORTANT: $2 is the word being completed, and
-# IPMORTANT: $3 is the word preceding the word being completed,
-# IMPORTANT: $4,$5,$6.. will always be empty.
-
 # dynamic
 _kes_dynamic()
 {
-	# DEBUG:
-	# echo -e "\n 0 $0, 1 $1, 2 $2, 3 $3"
+	echo -e "\n 0 $0, 1 $1, 2 $2, 3 $3, 4 $4"
 	# fyi: $0 IS ALWAYS `-bash` or `bash`
 	# echo -e "\n ARGS(received): $@"
 
-	# $3 is word being completed, so first of all we complete 
-	if [[ $3 = "kes" ]]; then
+	if [[ $3 = "-c" ]]; then
+		# GETTING CONTAINER NAME COMPLETION
+		# echo entered here...
+		podName=$(cat /tmp/cache-pod-name)
+		# echo $podName
+		# relatedContainers=$(kc get po $podName -o jsonpath="{.spec.containers[*].name}")
+		set -x
+		relatedContainers=$(kc get po $podName -o jsonpath="{.spec.containers[*].name}" | sed 's/ /\n/g' | grep ^$2)
+		# with sed i am conveting text like "nats reloader metrics" to three lines having each work in its own line.
+		# echo conts: $relatedContainers
+		for n in $relatedContainers; do
+			COMPREPLY+=("$n")
+		done
+	elif [[ $2 = "-c" ]]; then
+		COMPREPLY=("-c")
+		return
+	else
 		# GETTING POD NAME COMPLETION
 		items=$(kc get po | tail -n+2 | awk '{print $1}' | grep ^$2)
 		# `tail -n+2` means start from line 2 till end of file.
@@ -27,16 +32,9 @@ _kes_dynamic()
 		for n in $items; do
 			COMPREPLY+=("$n")
 		done
-	else
-		podName=$3
-		# echo $podName
-		# relatedContainers=$(kc get po $podName -o jsonpath="{.spec.containers[*].name}")
-		relatedContainers=$(kc get po $podName -o jsonpath="{.spec.containers[*].name}" 2> /dev/null | sed 's/ /\n/g' | grep ^$2)
-		# with sed i am conveting text like "nats reloader metrics" to three lines having each work in its own line.
-		# echo relatedContainers: $relatedContainers
-		for n in $relatedContainers; do
-			COMPREPLY+=("$n")
-		done
+
+		# This will help us track the pod name we are dealing with when we wanna get container name of pod.
+		echo $items > /tmp/cache-pod-name
 	fi
 }
 
@@ -45,8 +43,8 @@ complete -F _kes_dynamic kes
 # 1. CODE FOR KES IS DEFINED IN ~/.BASH_ALIASES FILE AREADY.
 # kes(){
 # 	# kes is autocompleted with my custom made autocompletion script.
-# 	echo +kc exec -it $1 -c $2 -- sh
-# 	kc exec -it $1 -c $2 -- sh
+# 	echo +kc exec -it "$@" -- sh
+# 	kc exec -it "$@" -- sh
 # }
 
 # 2. If you only want to test, you can test autocompletion via `./kes` script execution as well.
