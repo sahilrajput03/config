0bba067fe696b6db846f47d82be7ac06c7638118
Sahil Rajput <sahilrajput03@gmail.com>
2022-04-15 01:53:34 +0530

    Update.

--- scripts-in-use/battery-alert/battery-status.sh
+++ scripts-in-use/battery-alert/battery-status.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 # src: https://www.2daygeek.com/linux-low-full-charging-discharging-battery-notification/
-
 while true
 do
 	battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
 	discharging=`acpi | grep -o Discharging` # Text Discharging is returned if discharging.
 	# if [ $c1 ]; then echo Yes charging; fi
+
 	if [ $battery_level -ge 95 ]; then
-		echo 'yoyoy'
 		notify-send "Battery Full - ${battery_level}%" "Please unplug the charger.\n\nHave a nice day!"
-		for i in {1..2}; do ~/scripts-in-use/battery-alert/beepSound.sh; done
+		# for i in {1..2}; do $(dirname $0)/beepSound.sh; done
+		for i in {1..2}; do paplay /usr/share/sounds/freedesktop/stereo/bell.oga; done
+			# List all sounds:
+			# ls /usr/share/sounds/freedesktop/stereo
+			# paplay /usr/share/sounds/freedesktop/stereo/bell.oga
       		# paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
 		sleep 660 # i.e., sleep for 11 mins.
 	elif [ $battery_level -le 20 ] && [ $discharging ]; then
+		# echo batttery less than 20... testing only...
 		# LOG:01: This code only executed if battery is discharging ( not plugged in).
       		notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
-		for i in {1..3}; do ~/scripts-in-use/battery-alert/beepSound.sh; done
+			for i in {1..3}; do $(dirname $0)/beepSound.sh; done
       		# paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
 			# LOG:01: This code only executed if battery is discharging ( not plugged in).
 			# Hibernate if battery drops 14%.
@@ -23,7 +27,10 @@ do
 				systemctl hibernate
 			fi
 	else
-		echo "Battery is less than 20 percent but its plugged in...!"
+		echo "::Nothing to do because either battery percentage is
+1. less than 20 percent but its plugged in...!
+2. between 20 and 95 percent."
 	fi
+	echo ::Sleeping for 1 minute.
 	sleep 60
 	done
--- scripts-in-use/battery-alert/beepSound.sh
+++ scripts-in-use/battery-alert/beepSound.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-paplay /home/array/scripts-in-use/battery-alert/beep-sound-8333.wav
+paplay "$(dirname $0)/beep-sound-8333.wav"
 # for paplays's running via cronjob without error i fixed via: https://askubuntu.com/a/172888/702911 , just by adding ```autospawn = yes```
