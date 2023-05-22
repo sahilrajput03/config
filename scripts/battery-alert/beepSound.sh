ff27663a6fd32fb3ca1efcab51849aa7dc225d02
Sahil Rajput <sahilrajput03@gmail.com>
2023-05-22 05:53:38 +0530

    remove paplay and add cvlc to play audio clip

--- scripts/battery-alert/beepSound.sh
+++ scripts/battery-alert/beepSound.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-paplay "$(dirname $0)/beep-sound-8333.wav"
+# paplay "$(dirname $0)/beep-sound-8333.wav"
+# Using vlc's cli
+cvlc --play-and-exit "$(dirname $0)/beep-sound-8333.wav"
 # for paplays's running via cronjob without error i fixed via: https://askubuntu.com/a/172888/702911 , just by adding ```autospawn = yes```
