d78dd3fbda1fcdfed04d67704d1118e6d78ef315
Sahil Rajput <sahilrajput03@gmail.com>
2023-06-03 21:55:41 +0530

    update battery alert service.

--- scripts/battery-alert/battery-status.sh
+++ scripts/battery-alert/battery-status.sh
@@ -8,33 +8,22 @@ while true; do
 	MINIMUM_LEVEL=14
 	MAXIMUM_LEVEL=85
 
+	# Helful Logs to check charging and discharging
 	# if [ -z $discharging ]; then echo "Battery is charging"; fi
 	# if [ $discharging ]; then echo "Battery is discharging"; fi
 
-	# If batter is greater than `MAXIMUM_LEVEL` and battery is charging (-z $discharging) then alert the user
 	if [ $battery_level -ge $MAXIMUM_LEVEL ] && [ -z $discharging ]; then
+		# Battery is greater than `MAXIMUM_LEVEL` and charging (-z $discharging)
 		notify-send --urgency=CRITICAL "Battery Full - ${battery_level}%" "Please unplug the charger.\n\nHave a nice day!"
-		# for i in {1..2}; do $(dirname $0)/beepSound.sh; done
-		# better vv
-		# for i in {1..2}; do paplay /usr/share/sounds/freedesktop/stereo/bell.oga; done
-		# Below one is even better:
-		# paplay /home/array/scripts-media/Sounds/7_unplug-charger.wav
-		# Using `cvlc`
 		cvlc --play-and-exit /home/array/scripts-media/Sounds/7_unplug-charger.wav
-		# List all sounds:
-		# ls /usr/share/sounds/freedesktop/stereo
-		# paplay /usr/share/sounds/freedesktop/stereo/bell.oga
-		# paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
 		# Sleep for some time:
 		sleep 600
 	elif [ $battery_level -le 20 ] && [ $discharging ]; then
-		# echo batttery less than 20... testing only...
-		# LOG:01: This code only executed if battery is discharging ( not plugged in).
+		# Battery is discharging
 		notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
 		for i in {1..3}; do $(dirname $0)/beepSound.sh; done
 		# paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
-		# LOG:01: This code only executed if battery is discharging ( not plugged in).
-		# Hibernate if battery drops below `MINIMUM_LEVEL`.
+		# Hibernate when battery drops below `MINIMUM_LEVEL`.
 		if [ $battery_level -le $MINIMUM_LEVEL ]; then systemctl hibernate; fi
 	else
 		echo "
@@ -47,3 +36,12 @@ LOG - HAPPY TIME
 	echo "LOG - Sleeping for 1 minute."
 	sleep 60
 done
+
+# NOTES -
+# for i in {1..2}; do $(dirname $0)/beepSound.sh; done
+# for i in {1..2}; do paplay /usr/share/sounds/freedesktop/stereo/bell.oga; done
+# paplay /home/array/scripts-media/Sounds/7_unplug-charger.wav
+# List all sounds:
+# ls /usr/share/sounds/freedesktop/stereo
+# paplay /usr/share/sounds/freedesktop/stereo/bell.oga
+# paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
