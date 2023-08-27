49ee1817ae5fcf3abff719caf6185eb49ac9cdbc
Sahil Rajput <sahilrajput03@gmail.com>
2023-08-27 07:38:14 +0530

    update

--- scripts/battery-alert/beepSound.sh
+++ scripts/battery-alert/beepSound.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # paplay "$(dirname $0)/beep-sound-8333.wav"
+
 # Using vlc's cli
 cvlc --play-and-exit "$(dirname $0)/beep-sound-8333.wav"
 # for paplays's running via cronjob without error i fixed via: https://askubuntu.com/a/172888/702911 , just by adding ```autospawn = yes```
