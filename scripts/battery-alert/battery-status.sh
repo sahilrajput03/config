1cb5ae5fcf491f83d607f03a957d6b9e55e3d855
Sahil Rajput <sahilrajput03@gmail.com>
2023-09-03 13:53:55 +0530

    Update.

--- scripts/battery-alert/battery-status.sh
+++ scripts/battery-alert/battery-status.sh
@@ -5,7 +5,7 @@ MINIMUM_LEVEL=14
 MAXIMUM_LEVEL=95
 BATTERY_LOW_NOTIFICATION_RECHECK_INTERVAL=60  # In seconds
 BATTERY_FULL_NOTIFICATION_RECHECK_INTERVAL=60 # In seconds
-REPLACE_NOTIFICAION_ID=001
+BATTERY_HAPPY_RECHECK_INTERVAL=60             # In seconds
 
 while true; do
 	battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)')
@@ -13,16 +13,17 @@ while true; do
 
 	if [ $battery_level -ge $MAXIMUM_LEVEL ] && [ -z $discharging ]; then
 		# Battery is greater than `MAXIMUM_LEVEL` and charging (-z $discharging)
-		notify-send -r $REPLACE_NOTIFICAION_ID --urgency=CRITICAL "Battery Full - ${battery_level}%" "Please unplug the charger.\n\nHave a nice day!"
+		notify-send --urgency=CRITICAL "Battery Full - ${battery_level}%" -t $(($BATTERY_FULL_NOTIFICATION_RECHECK_INTERVAL * 1000)) "Please turn off the switch of charger \n on the wall.\n\nHave a nice day!"
 		cvlc --play-and-exit /home/array/scripts-media/Sounds/7_unplug-charger.wav
 		# Sleep for some time:
 		sleep $BATTERY_FULL_NOTIFICATION_RECHECK_INTERVAL
 	elif [ $battery_level -le $MINIMUM_LEVEL ] && [ $discharging ]; then
 		# Battery is discharging (notification timeout -t in milliseconds)
-		notify-send -r $REPLACE_NOTIFICAION_ID --urgency=CRITICAL -t $(($BATTERY_LOW_NOTIFICATION_RECHECK_INTERVAL * 1000)) "Battery Low" "Level: ${battery_level}%"
+		notify-send --urgency=CRITICAL -t $(($BATTERY_LOW_NOTIFICATION_RECHECK_INTERVAL * 1000)) "Battery Low" "Level: ${battery_level}%"
 		for i in {1..3}; do $(dirname $0)/beepSound.sh; done
 		# Hibernate when battery drops below `MINIMUM_LEVEL`.
 		if [ $battery_level -le $MINIMUM_LEVEL ]; then systemctl hibernate; fi
+		sleep $BATTERY_LOW_NOTIFICATION_RECHECK_INTERVAL
 	else
 		echo "
 ======
@@ -33,9 +34,9 @@ LOG - HAPPY TIME
 2. Between $MINIMUM_LEVEL and $MAXIMUM_LEVEL percent.
 3. More than $MAXIMUM_LEVEL and discharging.
 "
+		echo "LOG - Sleeping for 1 minute."
+		sleep $BATTERY_HAPPY_RECHECK_INTERVAL
 	fi
-	echo "LOG - Sleeping for 1 minute."
-	sleep $BATTERY_LOW_NOTIFICATION_RECHECK_INTERVAL
 done
 
 # NOTES -
